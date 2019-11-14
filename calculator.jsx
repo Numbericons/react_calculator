cd75199e9f02562be7995650494621c608663fa1
Zachary <zachjoliver@gmail.com>
2019-11-13 23:43:52 -0800

    Calculator working for basic functions

--- calculator.jsx
+++ calculator.jsx
@@ -3,15 +3,69 @@ import React from "react";
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    // your code here
+    this.state = {
+      num1: "",
+      num2: "",
+      result: 0
+    };
+    this.setNum1 = this.setNum1.bind(this);
+    this.setNum2 = this.setNum2.bind(this);
+    this.calc = this.calc.bind(this);
+    this.reset = this.reset.bind(this);
+  };
+
+  setNum1(e) {
+    e.preventDefault();
+    this.setState({num1: e.target.value})
   }
 
-  // your code here
+  setNum2(e) {
+    e.preventDefault();
+    this.setState({num2: e.target.value})
+  }
+  
+  calc(e) {
+    e.preventDefault();
+    const operation = e.target.value;
+    const first = parseInt(this.state.num1);
+    const second = parseInt(this.state.num2);
+    let opResult;
+    switch (operation) {
+      case "+":
+        opResult = first + second;
+        break;
+      case "-":
+        opResult = first - second;
+        break;
+      case "*":
+        opResult = first * second;
+        break;
+      case "/":
+        opResult = first / second;
+        break;
+    }
+    this.setState( {result: opResult } );
+  }
+
+  reset(e) {
+    this.setState( {
+      num1: "",
+      num2: "",
+      result: 0
+    })
+  }
 
   render() {
     return (
       <div>
-        <h1>Hello World</h1> // replace this with your code
+        Result: <h1>{this.state.result}</h1>
+        <input onChange={this.setNum1} value={this.state.num1}/>
+        <input onChange={this.setNum2} value={this.state.num2}/>
+        <button onClick={this.calc} value="+">+</button>
+        <button onClick={this.calc} value="-">-</button>
+        <button onClick={this.calc} value="*">*</button>
+        <button onClick={this.calc} value="/">/</button>
+        <button onClick={this.reset}>Reset</button>
       </div>
     );
   }
